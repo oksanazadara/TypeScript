fc765c161a34cab5cdd8ab765e6d724e8b8b8220
oksanazadara <o_zadara@ukr.net>
2026-01-05 18:25:43 +0200

    Data type boolean

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-// for(let i = 0; i < 10; i+=2) {
-//     if (i%2 === 0) {
-//     console.log(`Число ${i}`);
-//     }    
-// }
-// console.log(this.document)
-const form = document.querySelector("#factorial-form");
-const steplist = document.querySelector("#factorial-steps");
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    steplist.textContent = "";
-    const numberInput = document.querySelector("#number");
-    const number = Number(numberInput.value);
-    let factorial = 1;
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-        console.log(factorial);
-        const listItem = document.createElement('li');
-        listItem.textContent = `Добуток ${factorial}`;
-        steplist.appendChild(listItem);
-    }
-});
+const hasDriverLicense = true;
+if (hasDriverLicense) {
+    console.log('Користувач може управляти транспортом');
+}
+else {
+    console.log('Користувач не має права управляти транспортом');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,7 @@
+const hasDriverLicense: boolean = true;
 
-
-// for(let i = 0; i < 10; i+=2) {
-//     if (i%2 === 0) {
-        
-//     console.log(`Число ${i}`);
-//     }    
-// }
-// console.log(this.document)
-
-const form = document.querySelector("#factorial-form") as HTMLFormElement;
-const steplist = document.querySelector("#factorial-steps") as HTMLOListElement;
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    steplist.textContent = "";
-
-    const numberInput = document.querySelector("#number") as HTMLInputElement;
-
-    const number: number = Number(numberInput.value);
-
-    let factorial: number = 1;
-
-    for (let i = 1; i <= number; i++) {
-        factorial *= i;
-        console.log(factorial);
-
-        const listItem: HTMLLIElement = document.createElement('li');
-        listItem.textContent = `Добуток ${factorial}`;
-        steplist.appendChild(listItem);
-        
-    }
-    
-
-})
-
+if (hasDriverLicense) {
+    console.log('Користувач може управляти транспортом');
+} else {
+    console.log('Користувач не має права управляти транспортом');
+}
