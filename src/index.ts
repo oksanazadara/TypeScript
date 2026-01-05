3a3d6a0c9d953e7f6784ad6a290de85366885294
oksanazadara <o_zadara@ukr.net>
2026-01-05 17:52:31 +0200

    Factorial Calculator

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello')
+
+
+// for(let i = 0; i < 10; i+=2) {
+//     if (i%2 === 0) {
+        
+//     console.log(`Число ${i}`);
+//     }    
+// }
+// console.log(this.document)
+
+const form = document.querySelector("#factorial-form") as HTMLFormElement;
+const steplist = document.querySelector("#factorial-steps") as HTMLOListElement;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    steplist.textContent = "";
+
+    const numberInput = document.querySelector("#number") as HTMLInputElement;
+
+    const number: number = Number(numberInput.value);
+
+    let factorial: number = 1;
+
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+        console.log(factorial);
+
+        const listItem: HTMLLIElement = document.createElement('li');
+        listItem.textContent = `Добуток ${factorial}`;
+        steplist.appendChild(listItem);
+        
+    }
+    
+
+})
+
